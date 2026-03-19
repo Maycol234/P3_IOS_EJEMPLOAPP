cda2d919dc8622dbf978f0a2f4143495eb799f44
Maycol234 <kevinlopezacevedo905@gmail.com>
2026-03-19 10:21:04 -0600

    Delete css/js directory

--- css/js/app.js
+++
@@ -1,9 +0,0 @@
-funtion mostrarSeccion(id) {
-  let secciones = document.querySelectorAll(".seccion");
-
-secciones.forEach(function(seccion) {
-  seccion.classList.remove("activa");
-});
-
-document.getElementById(id).classList.add("activa");
-}
