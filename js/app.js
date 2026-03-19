d4c65f78d1d61f0b7e7f88178fb1a720a14335bf
Maycol234 <kevinlopezacevedo905@gmail.com>
2026-03-19 10:24:07 -0600

    Add function to show section by ID

---
+++ js/app.js
@@ -0,0 +1,10 @@
+function mostrarSeccion(id) {
+  let secciones = document.querySelectorAll(".seccion");
+
+secciones.forEach(funnction(seccion) {
+  seccion.classList.remove("activa);
+                           });
+  document.getElementById(id).classList.add("activa");
+}
+                           
+                           
